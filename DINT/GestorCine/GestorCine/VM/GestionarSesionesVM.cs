2c9cb2ad37efb5be4a61a5969aec7892e4a37a31
EfrenSabater <efren.sabater@iesdoctorbalmis.com>
2021-02-18 23:59:33 +0100

    Empezando el layout y lógica de GestorSesiones

---
+++ DINT/GestorCine/GestorCine/VM/GestionarSesionesVM.cs
@@ -0,0 +1,25 @@
+﻿using GestorCine.POJO;
+using GestorCine.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorCine.VM
+{
+    class GestionarSesionesVM : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<Sesion> ListaSesiones { get; set; }
+        public Sesion NuevaSesion;
+        public Sesion SesionSeleccionada;
+        public ServicioSesion _servicio;
+
+        public GestionarSesionesVM() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
